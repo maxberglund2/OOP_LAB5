e7373ec0bd6fcaca70805cc09ed926863fc5e0b6
Greed <max.berglund2004@gmail.com>
2024-10-15 10:35:14 +0200

    update

--- OOP_LAB5/Program.cs
+++ OOP_LAB5/Program.cs
@@ -1,18 +1,18 @@
 ﻿class Program {
-class Cricle
-{
-    int Radius { get; set; }
+    class Cricle
+    {
+        int Radius { get; set; }
 
-    public Cricle(int radius)
-    {
-        Radius = radius;
+        public Cricle(int radius)
+        {
+            Radius = radius;
+        }
+        public void GetArea()
+        {
+            double area = 3.14 * Radius * Radius;
+            Console.WriteLine("Area of circle is: " + area);
+        }
     }
-    public void GetArea()
-    {
-        double area = 3.14 * Radius * Radius;
-        Console.WriteLine("Area of circle is: " + area);
-    }
-}
     static void Main()
     {
         Cricle c = new Cricle(5);
